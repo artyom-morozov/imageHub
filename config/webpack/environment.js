f440db9a90959c7000ad10a149b96141924f7f55
Artyom Morozov <47092586+Sviktam1@users.noreply.github.com>
2021-06-03 01:53:35 -0400

    Fix heroku failure

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
-import { ProvidePlugin } from 'webpack'
-environment.plugins.append('Provide',
-    new ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['@popperjs/core', 'default']
-    })
-)
-export default environment
+const webpack = require("webpack") 
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+module.exports = environment
